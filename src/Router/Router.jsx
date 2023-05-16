3ec7950c336ca463181650621a8f95f9cea2f01f
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-05-16 15:39:11 +0600

    set carousel

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Pages/Main/Main";
 import Error from "../Pages/Error/Error";
+import HomeComponents from "../Pages/Home_Components/HomeComponents";
 
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
-        errorElement: <Error></Error>
+        errorElement: <Error></Error>,
+        children: [
+            {
+                path: '/',
+                element: <HomeComponents></HomeComponents>
+            }
+        ]
     }
 ]);
